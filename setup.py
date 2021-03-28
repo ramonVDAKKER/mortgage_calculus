d5b8ee0ada3f711c2cd99dad5a4d0b62009b07f2
Ramon van den Akker <ramon.vdakker@gmail.com>
2021-03-28 18:08:29 +0200

    basis functionality

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+def readme():
+      with open("README.md") as f:
+            return f.read()
+
+setup(name="mortgage_calculus",
+      version="0.0.1",
+      description="Tools for the analysis of mortgages",
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      classifiers=[
+        "License :: OSI Approved :: MIT License"
+      ],
+      keywords="mortgage cashflows annuity interest",
+      url="http://github.com/ramonVDAKKER/mortgage_calculus",
+      author="Ramon van den Akker",
+      author_email="ramon@vandenakker.tech",
+      license="MIT",
+      packages=find_packages(include=["mortgage_calculus"]),
+      install_requires=["numpy", "pandas", "typing"],
+      python_requires= ">=3.7",
+      setup_requires=["pytest-runner", "flake8"],
+      tests_require=["pytest"],
+     # extras_require={"dev": ["pywin32 >=1.0", "ipykernel", "notebook", "jupyterlab"]}
+      )
